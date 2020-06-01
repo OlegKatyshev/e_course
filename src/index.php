f31a7820af27dbff2817e546d4663bc622f07dbe
Олег <oleg_kat@bk.ru>
2020-06-01 20:01:32 +0300

    finish project conf

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Excel</title>
+    <link rel="shourtcut icon" href="./dist/favicon.png">
+</head>
+<body>
+    <div class="app"></div>
+</body>
+</html>
